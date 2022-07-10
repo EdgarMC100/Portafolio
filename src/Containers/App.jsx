da60fd9203ab5577c6f1c2ec35fdee9938dec24f
Edgar Ubaldo Morales Cortes <edgarcortes@Edgars-Mac-mini.local>
2022-07-09 20:31:20 -0500

    dependencies updated and styles added

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,17 +1,20 @@
 //Componente principal que contiene la estructura del proyecto
-import React from 'react';
-import {createGlobalStyle} from 'styled-components';
-import Main from '../Components/Main';
-import Sidebar from '../Components/Sidebar';
-import Info from '../Components/Info';
-import About from '../Components/About';
-import Education from '../Components/Education';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import Main from "../Components/Main";
+import Sidebar from "../Components/Sidebar";
+import Info from "../Components/Info";
+import About from "../Components/About";
+import Education from "../Components/Education";
 // import Experience from '../Components/Experience';
 // import Certificates from '../Components/Certificates';
-import Skills from '../Components/Skills';
-import useGetData from '../Hooks/useGetData';
+import Skills from "../Components/Skills";
+import useGetData from "../Hooks/useGetData";
 
 const GlobalStyle = createGlobalStyle`
+    html{
+    //  font-size: 62.5%;    
+    }
     body{
         font-family: 'Lato', sans-serif;
         margin:0;
@@ -20,27 +23,29 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 const App = () => {
-    const data = useGetData();
-    console.log(data);
-    return data.length == 0 ? <h1>Cargando....</h1> : (
-        <Main>
-            <GlobalStyle/>
-            <Sidebar>
-                <About
-                    avatar={data.avatar}
-                    name={data.name}
-                    profession={data.profession}
-                    bio={data.bio}
-                    address={data.address}
-                    social={data.social}
-                />
-            </Sidebar>
-            <Info>
-                <Education data ={data.education}/>
-                <Skills data = {data.skills}/>
-            </Info>
-        </Main>
-    );
-}
+  const data = useGetData();
+  console.log(data);
+  return data.length == 0 ? (
+    <h1>Cargando....</h1>
+  ) : (
+    <Main>
+      <GlobalStyle />
+      <Sidebar>
+        <About
+          avatar={data.avatar}
+          name={data.name}
+          profession={data.profession}
+          bio={data.bio}
+          address={data.address}
+          social={data.social}
+        />
+      </Sidebar>
+      <Info>
+        <Education data={data.education} />
+        <Skills data={data.skills} />
+      </Info>
+    </Main>
+  );
+};
 
 export default App;
--- src/Styled/H2Styled.jsx
+++ src/Styled/H2Styled.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const StyledH2 = styled.h2
-`
-    font-familu: 'Roboto, san-serif';
-    font-weight:300;
-    letter-spacing: .8px;
-    margin: 1em 0 0 0;
-    color: #C2185b;
+const StyledH2 = styled.h2`
+  font-family: "Roboto, san-serif";
+  font-weight: 300;
+  letter-spacing: 0.8px;
+  margin: 1em 0 1em 0;
+  color: #c2185b;
+  text-align: center;
 `;
 
-const H2Styled = ({name})=><StyledH2>{name}</StyledH2>;
+const H2Styled = ({ name }) => <StyledH2>{name}</StyledH2>;
 
 export default H2Styled;
